945a9df6f9ff3a0cbdbad31f3c4fe612562fa839
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-16 14:23:50 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
       <header className="App-header">
       <h1 className="text-4xl font-bold underline">
           Kiddo!
-      </h1>
-      <button class="btn btn-info">Info</button>
+      </h1><br/>
+      <button className="btn">
+          Button
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+      </button>
       </header>
     </div>
   );
