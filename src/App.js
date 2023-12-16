6680cfbf834c84f5de0ab79dac3f1184a3d5926f
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-16 19:29:11 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
       </button><br/>
       <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
+      <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+      <div className="card-body">
+        <h2 className="card-title">New album is released!</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+      <div className="card-actions justify-end">
       <button className="btn btn-primary">Listen</button>
     </div>
   </div>
