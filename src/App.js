4f42bee48ebd1f2c17f22355e0a15c7c2628e4a4
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-16 14:19:27 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1 className="text-4xl font-bold underline">
           Kiddo!
       </h1>
+      <button class="btn btn-info">Info</button>
       </header>
     </div>
   );
