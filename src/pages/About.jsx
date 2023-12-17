448b3716f5a56db4e5608a34736c3e14ee529467
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-18 01:50:10 +0530

    modified:   .env 	modified:   src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,31 @@
 function About() {
   return (
-    <>
-      <h1 className='text-6xl mb-4'>Github Finder</h1>
-      <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
-          React Front To Back
-        </a>{' '}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
-      </p>
-      <p className='text-lg text-gray-400'>
-        Version <span className='text-white'>1.0.0</span>
-      </p>
-      <p className='text-lg text-gray-400'>
-        Layout By:
-        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-          Hassib Moddasser
-        </a>
-      </p>
-    </>
-  )
+    <div className="container mx-auto my-8 p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-6xl mb-4 text-center text-gray-800">GitHub Finder</h1>
+        <p className="text-lg text-gray-600">
+            The GitHub Finder is an intuitive, user-friendly application designed 
+            to streamline the process of searching for GitHub profiles. Leveraging 
+            the power of the GitHub API, this tool allows users to easily find and 
+            display detailed information about GitHub users, including their repositories, 
+            followers, and more.
+        </p>
+        <p className="text-lg text-gray-600 mt-4">
+            Built with the latest web technologies, the GitHub Finder incorporates 
+            <strong> React </strong> for efficient UI rendering and <strong> React Router </strong> 
+            for seamless navigation. To enhance the user experience, the application is styled 
+            with <strong> Tailwind CSS </strong>, a utility-first CSS framework that enables 
+            responsive and customizable design elements. Furthermore, <strong> DaisyUI </strong> 
+            is integrated to provide a collection of prebuilt UI components, making the interface 
+            both attractive and user-friendly.
+        </p>
+        <p className="text-lg text-gray-600 mt-4">
+            Whether you're a developer seeking to explore the work of your peers, or 
+            a recruiter looking for potential candidates, GitHub Finder offers a 
+            convenient and efficient way to browse GitHub profiles. Dive in and 
+            experience the power of streamlined GitHub exploration!
+        </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
