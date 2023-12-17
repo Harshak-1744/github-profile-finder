e699ebb342f6820d06724989360dca1b0fcba7e8
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-17 06:21:13 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.js 	new file:   src/components/layouts/Navbar.jsx 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	modified:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="text-4xl font-bold underline">
-          Kiddo!
-      </h1><br/>
-      <button className="btn">
-          Button
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-      </button><br/>
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-      <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-      <div className="card-body">
-        <h2 className="card-title">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-      <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
+    <Router>
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main>Content</main>
     </div>
-  </div>
-</div>
-      </header>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+  
